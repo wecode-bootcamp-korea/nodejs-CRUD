cd987abcc6e8b71e46ab7ebfd5de9a9137960dff
samankey <samankeyc@gmail.com>
2021-06-22 01:08:46 +0900

    Add : express를 이용한 api 제작

---
+++ sendPostsList.js
@@ -0,0 +1,17 @@
+const sendPostsList = (req, res) => {
+  res.json({
+  "data" : [{
+    "id" : 1,
+    "koreanName" : "나이트로 바닐라크림",
+    "englishName" : "Nitro Vanilla Cream",
+    "category" : "콜드 브루"
+  }, {
+    "id" : 2,
+    "koreanName" : "아이스 카페 아메리카노",
+    "englishName" : "Ice Cafe Americano",
+    "category" : "에스프레소"
+  }]
+})
+}
+
+export { sendPostsList }
